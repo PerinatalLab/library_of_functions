271d3ab58bcc0e16075fbf33ec299b1b8fb26d43
Jonas B <jonas.bacelis@gmail.com>
2015-06-30 12:40:14 +0200

    some bug was fixed, comments added (?)

--- annotate_PLINK_results.R
+++ annotate_PLINK_results.R
@@ -2,21 +2,9 @@
 
 # this function annotates SNP based on their functionality or LD with functional SNPs
 
-
-# only without PROM deliveries
-#rez_dir = "~/Biostuff/MOBA_GESTAGE_GWAS/PERMUT/EXTRACTED_DIGESTED_RESULTS/nonPROM/"
-#results =paste(rez_dir, "MomsKids_nonPROM_PermAsympt_5608_hg19.RData",sep="")
-#load(file=results)
-#a=get("moms")
-#a=a[order(a$ePxtrm),];  a=a[which(!is.na(a$ePxtrm)),]
-#out=a[,c("SNP","CHR","BP","ePxtrm")];  colnames(out)[4]="P"
-#out=out[1:20,]
-#head(out)
-
-
-fun = function(input) {  # SNP CHR  BP P
+annotatePLINKresults = function(input) {  # SNP CHR  BP P
         
-        snps="/Users/jb/Biostuff/hg19_HUMAN_GENES/snp129.attrib"
+        snps="/Users/jb/Biostuff/hg19_HUMAN_GENES/snp129.attrib" # downloaded from PLINK website
         temp_dir="~/Biostuff/MOBA_GESTAGE_GWAS/PERMUT/temp_dir/"
         tempFile1=paste(temp_dir,"temporaryFile_toPLINK_deleteME.txt",sep="")
         tempFile2=paste(temp_dir,"temporaryFile_fromPLINK_deleteME",sep="")
