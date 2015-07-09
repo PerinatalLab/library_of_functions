ee9b223024321475d5f6f4435dbdd623235be78d
Jonas B <jonas.bacelis@gmail.com>
2015-07-09 15:55:26 +0200

    added "stringsAsFactors=F" to prevent factorization of geneSet names

--- extract_tables_from_INRICH_output.R
+++ extract_tables_from_INRICH_output.R
@@ -2,8 +2,8 @@
 
 getINRICHpvals = function(inrinchOutputFile) {
         temp88=read.table(pipe(paste("gawk -v OFS='\t' '$0~/Main Analysis Results/{pr=1} ",
-                                     "$0~/------/{next} $0~/^$/{pr=0} pr==1{print $2,$3,$4,$5,$6,$7,$8}' ",
-                                     inrinchOutputFile,sep="")),sep="\t",skip=1,h=T)
+        "$0~/------/{next} $0~/^$/{pr=0} pr==1{print $2,$3,$4,$5,$6,$7,$8}' ",
+        inrinchOutputFile,sep="")),sep="\t",skip=1,h=T,stringsAsFactors=F)
         temp88
 }
 getINRICHpvals(input_obj)
@@ -14,8 +14,9 @@ print("(input required: full address and name of INRICH output file)",quote=F)
 
 getINRICHgenes = function(inrinchOutputFile,GeneSetName) {
         temp99=read.table(pipe(paste("gawk -v OFS='\t' '$0~/Interval-Gene-Target/{pr=1} ",
-                                     "$0~/------/{next} $0~/^$/{pr=0} pr==1{print $2,$5,$6,$7\" \"$8,$9}' ",
-                                     inrinchOutputFile ," | awk '$0~/",GeneSetName,"/{print $1,$2,$3,$6}'",sep="")),sep=" ")
+        "$0~/------/{next} $0~/^$/{pr=0} pr==1{print $2,$5,$6,$7\" \"$8,$9}' ",
+        inrinchOutputFile ," | awk '$0~/",GeneSetName,"/{print $1,$2,$3,$6}'",sep="")),
+        sep=" ",stringsAsFactors=F)
         temp99
 }
 print("Function \"getINRICHgenes\" extracts names of enriched genes for a particular geneSet from INRINCH output file.",quote=F)
