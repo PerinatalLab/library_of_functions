75ff91c33c5ec84e1d1cb493883fc2f40450b554
jjuod <j.juodakis@gmail.com>
2016-11-02 17:43:57 +0100

    added function for symmetric sampling

--- sample_dataframe_uniformly.R
+++ sample_dataframe_uniformly.R
@@ -26,7 +26,7 @@ makeUniformSample = function(df, nameX, nameY, binsX, binsY, nsample){
         df$binX = cut(df[,nameX], binsX)
         df$binY = cut(df[,nameY], binsY)
         
-        ns = group_by(df, binX, binY) %>% summarize(n=n())
+        ns = group_by(df, binX, binY) %>% summarize(n = n())
         df = inner_join(df, ns, by=c("binX", "binY"))
         df = group_by(df, binX, binY)
         
@@ -37,10 +37,35 @@ makeUniformSample = function(df, nameX, nameY, binsX, binsY, nsample){
         
         if(min(ns$n) < nsample){
                 takeSample = filter(df, n>nsample) %>% sample_n(nsample)
-                takeEverything = filter(df, n<=nsample)
+                takeEverything = filter(df, n <= nsample)
                 df = bind_rows(takeSample, takeEverything)
         } else {
                 df = sample_n(df, nsample)
         }
         return(as.data.frame(ungroup(df)))
+}
+
+### takes a sample that is symmetric around a desired axis
+# by Julius J. 2016 Nov 02
+
+# USAGE: makeSymmetricSample(df, column, threshold)
+#     df      input data frame
+#     column    name of the column containing gest. age values (string)
+#     threshold produced sample will be symmetric around x=threshold axis (integer)
+
+makeSymmetricSample = function(df, column, threshold){
+        colnames(df)[colnames(df) == column] = "filtcol"
+        ns = filter(df, filtcol <= threshold) %>%
+                group_by(filtcol) %>%
+                summarize(n = n()) %>%
+                mutate(filtcol = 2*threshold - filtcol)
+        
+        belowThreshold = filter(df, filtcol < threshold)
+        df = inner_join(df, ns, by="filtcol") %>%
+                group_by(filtcol) %>%
+                do(sample_n(., .$n[1])) %>%
+                bind_rows(belowThreshold)
+        
+        colnames(df)[colnames(df) == "filtcol"] = column
+        return(df)
 }
